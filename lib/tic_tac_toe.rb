3c43c9ac5be15afb06fa402ee75a23734ff83aab
Ricky Rojas <rickyrojas19@gmail.com>
2019-05-16 03:01:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -104,10 +104,17 @@ class TicTacToe
   end
 end
 
-def play 
+def play
   until over? == true
     turn
   end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 end
   
 end
