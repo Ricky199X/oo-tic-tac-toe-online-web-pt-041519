32e50a010e433922afd48852fdb62dbde21d6f4a
Ricky Rojas <rickyrojas19@gmail.com>
2019-05-16 03:27:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,13 +38,9 @@ def position_taken?(index)
     end
   end 
 
-def valid_move?(index)
-    if !position_taken?(index) && index == 0...9
-      return true
-    else
-      false
-    end
-  end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+end
 
 def turn_count
   turn = 0
